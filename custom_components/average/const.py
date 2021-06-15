534ff3ced04294656f4fb5763bd53ce5062b4918
Andrey Khrolenok <andrey@khrolenok.ru>
2021-06-15 20:02:01 +0300

    Bump version to 2.1.0

--- custom_components/average/const.py
+++ custom_components/average/const.py
@@ -9,7 +9,7 @@ from datetime import timedelta
 # Base component constants
 NAME = "Average Sensor"
 DOMAIN = "average"
-VERSION = "2.0.3-alpha"
+VERSION = "2.1.0"
 ISSUE_URL = "https://github.com/Limych/ha-average/issues"
 
 STARTUP_MESSAGE = f"""
