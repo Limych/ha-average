ba8876a3e5198ab3c3310ac2c0e9338296e0af61
Andrey Khrolenok <andrey@khrolenok.ru>
2022-02-21 23:28:06 +0300

    Mark all constants as final

--- custom_components/integration_blueprint/const.py
+++ custom_components/integration_blueprint/const.py
@@ -1,13 +1,15 @@
 """Constants for integration_blueprint."""
 
 # Base component constants
-NAME = "Integration blueprint"
-DOMAIN = "integration_blueprint"
-VERSION = "0.1.0"
-ATTRIBUTION = "Data provided by http://jsonplaceholder.typicode.com/"
-ISSUE_URL = "https://github.com/Limych/ha-blueprint/issues"
+from typing import Final
 
-STARTUP_MESSAGE = f"""
+NAME: Final = "Integration blueprint"
+DOMAIN: Final = "integration_blueprint"
+VERSION: Final = "0.1.0"
+ATTRIBUTION: Final = "Data provided by http://jsonplaceholder.typicode.com/"
+ISSUE_URL: Final = "https://github.com/Limych/ha-blueprint/issues"
+
+STARTUP_MESSAGE: Final = f"""
 -------------------------------------------------------------------
 {NAME}
 Version: {VERSION}
@@ -18,24 +20,24 @@ If you have ANY issues with this you need to open an issue here:
 """
 
 # Icons
-ICON = "mdi:format-quote-close"
+ICON: Final = "mdi:format-quote-close"
 
 # Device classes
-BINARY_SENSOR_DEVICE_CLASS = "connectivity"
+BINARY_SENSOR_DEVICE_CLASS: Final = "connectivity"
 
 # Platforms
-BINARY_SENSOR = "binary_sensor"
-SENSOR = "sensor"
-SWITCH = "switch"
-PLATFORMS = [BINARY_SENSOR, SENSOR, SWITCH]
+BINARY_SENSOR: Final = "binary_sensor"
+SENSOR: Final = "sensor"
+SWITCH: Final = "switch"
+PLATFORMS: Final = [BINARY_SENSOR, SENSOR, SWITCH]
 
 # Configuration and options
-CONF_ENABLED = "enabled"
-CONF_USERNAME = "username"
-CONF_PASSWORD = "password"
+CONF_ENABLED: Final = "enabled"
+CONF_USERNAME: Final = "username"
+CONF_PASSWORD: Final = "password"
 
 # Defaults
-DEFAULT_NAME = DOMAIN
+DEFAULT_NAME: Final = DOMAIN
 
 # Attributes
-ATTR_INTEGRATION = "integration"
+ATTR_INTEGRATION: Final = "integration"
