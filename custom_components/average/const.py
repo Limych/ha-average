deb87362312ddd79e778f2a2a3707bab4eb2868f
Andrey Khrolenok <andrey@khrolenok.ru>
2022-05-07 21:47:25 +0300

    Bump version to 2.2.2

--- custom_components/average/const.py
+++ custom_components/average/const.py
@@ -10,7 +10,7 @@ from typing import Final
 # Base component constants
 NAME: Final = "Average Sensor"
 DOMAIN: Final = "average"
-VERSION: Final = "2.2.2-alpha"
+VERSION: Final = "2.2.2"
 ISSUE_URL: Final = "https://github.com/Limych/ha-average/issues"
 
 STARTUP_MESSAGE: Final = f"""
